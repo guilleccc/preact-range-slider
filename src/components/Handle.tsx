b8aa291493a8cb5e9506c8dd27f816b0e68220f9
Guillermo Constantino <guillermo.constantino@startsiden.no>
2018-09-27 13:50:47 +0200

    Removed tabIndex

--- src/components/Handle.tsx
+++ src/components/Handle.tsx
@@ -86,7 +86,6 @@ class Handle extends Component<HandleProps, HandleState>
 				class={classes}
 				style={style}
 				role="slider"
-				tabIndex={disabled ? undefined : -1}
 				aria-orientation={vertical ? 'vertical' : 'horizontal'}
 				aria-disabled={String( disabled )}
 				aria-valuemin={min}
